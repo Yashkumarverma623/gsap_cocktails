e0285168dd7552669bf3af4a2888f3e38902eca9
Yash Kumar Verma <yashkumarverma623@gmail.com>
2025-06-28 22:43:29 +0530

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { React } from "react";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import { gsap } from "gsap";
 gsap.registerPlugin(ScrollTrigger, SplitText);
+import Navbar from "./Components/Navbar.jsx";
+
 
 function App() {
  
 
   return (
     <>
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-8xl  text-indigo-400">Test GSAP</h1>
-    </div>
+    <main>
+      <Navbar/>
+    </main>
     </>
   )
 }
