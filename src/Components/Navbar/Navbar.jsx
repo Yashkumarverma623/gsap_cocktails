1f59393b9f61fa012e99fb9d1b3ef3746edc1b29
Yash Kumar Verma <yashkumarverma623@gmail.com>
2025-06-29 12:13:02 +0530

    full navbar commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import {navLinks} from "../Constrants/index.js";
-
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
 
 const Navbar = () => {
+
+  useGSAP(()=>{
+    const navTween = gsap.timeline({
+      scrollTrigger:{
+        trigger:"nav",
+        start: "bottom top",
+      }
+    })
+
+    navTween.fromTo('nav',{
+     backgroundColor: "transparent",
+    },{
+     backgroundColor: "#00000050",
+     backgroundFilter:"blur",
+     duration: 1,
+     ease:"power1.inOut"
+    })
+  }, [])
+
+
+
   return (
     <>
       <nav>
